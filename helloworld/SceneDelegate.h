44478d27cdbbab99a8e3478c3a1f8ac68fcb52cd
liangyongsen1 <liangyongsen1@wps.cn>
2021-04-22 16:16:32 +0800

    Initial Commit

---
+++ helloworld/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  helloworld
+//
+//  Created by kingsoft on 2021/4/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ helloworld/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  helloworld
+//
+//  Created by kingsoft on 2021/4/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
