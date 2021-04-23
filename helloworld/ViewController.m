a509f5e4c8edfd79b9eecbaaa6a9eac30a15cc4b
liangyongsen1 <liangyongsen1@wps.cn>
2021-04-23 09:13:57 +0800

    添加main

--- helloworld/ViewController.m
+++ helloworld/ViewController.m
@@ -16,8 +16,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    printf("hello");
+    //printf("hello\n");
+    
+    NSLog(@"Hello!");
 }
+
+extern int a=3;
+
+int main ( ){
+    int a = 2;
+    printf(a);
+    
+    
+    
+    return 0;
+}
+
+
 
 
 @end
